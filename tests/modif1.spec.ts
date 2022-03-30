80cd3f8b8291b1cf213bd1dc9caee9b649e37df5
Luciano Sekulic <alu0101022239@ull.edu.es>
2022-03-30 15:38:54 +0000

    subiendo carpetas

---
+++ tests/modif1.spec.ts
@@ -0,0 +1,14 @@
+import 'mocha';
+import { expect } from 'chai';
+import { } from '../src/modif1';
+import { } from '../src/modif1';
+import { } from '../src/modif1';
+
+
+
+describe('prueba modif1', () =>{
+  it('primer test', () => {
+
+  });
+    
+});
