25cd0b3cfe1c5015b2724c27c2550167908b0ff1
Luciano Sekulic <alu0101022239@ull.edu.es>
2022-03-31 10:41:00 +0000

    subiendo nodificacion

--- src/modif1.ts
+++ src/modif1.ts
@@ -1,5 +1,140 @@
 
+/**
+ * Se trabaja en pulgada
+ */
+export class imperialLength {
+  constructor(private result: number = 0) {}
 
-export class modif1 {
-    
+  getPulgadas():  number {
+
+    return this.result
+  }
+
+  getYardas() {
+    let result: number = 0
+    result = result * 0.0277778;
+    return this.result
+  }
+
+  getPies() {
+    let result: number = 0
+    result = result * 0.0833333
+    return this.result
+  }
+
+  getMillas() {
+    let result: number = 0
+    result = result * 0.000015782799999999997
+    return this.result
+  }
+}
+
+/**
+ * se trabaja oz
+ */
+export class imperialMasa {
+  constructor(private result: number) {}
+
+  getOnzas():  number {
+    return this.result
+  }
+
+  getLibras() {
+    let result: number = 0
+    result = result * 0.0625
+    return this.result
+  }
+
+  getPiedras() {
+    let result: number = 0
+    result = result * 0.00446429
+    return this.result
+  }
+
+  getCentenas() {
+    let result: number = 0
+    result = result * 0.000558036
+    return this.result
+  }
+
+  getToneladas() {
+    let result: number = 0
+    result = result * 0.000027901999999999998
+    return this.result
+  }
+}
+
+type conversion = {
+
+}
+
+
+/**
+ * Se trabaja en cm
+ */
+ export class metricLength {
+  constructor(private result: number = 0) {}
+
+  getcm():  number {
+
+    return this.result
+  }
+
+  getKM() {
+    let result: number = 0
+    result = result * 0.000009999999999999999
+    return this.result
+  }
+
+  getMetros() {
+    let result: number = 0
+    result = result * 0.01
+    return this.result
+  }
+
+}
+
+
+
+/**
+ * Se trabaja en gramos
+ */
+ export class metricMass {
+  constructor(private result: number = 0) {}
+
+  getGramos():  number {
+
+    return this.result
+  }
+
+  getKiogramos() {
+    let result: number = 0
+    result = result * 0.001
+    return this.result
+  }
+
+  getToneladas() {
+    let result: number = 0
+    result = result * 0.000001
+    return this.result
+  }
+
+}
+
+
+export class Adapter extends imperialLength {
+  constructor(private service: metricLength) {
+    super();
+  }
+
+  getData(): number {
+    const pulgada = this.service.getcm() * 2.54;
+    return pulgada
+  }
+/** 
+  getData1(): number {
+    const oz = this.service1.get() * 2.54;
+    return oz
+  }
+  */
 }
--- tests/modif1.spec.ts
+++ tests/modif1.spec.ts
@@ -1,14 +1,52 @@
 import 'mocha';
 import { expect } from 'chai';
-import { } from '../src/modif1';
-import { } from '../src/modif1';
-import { } from '../src/modif1';
-
-
+import { imperialLength } from '../src/modif1';
+import { imperialMasa } from '../src/modif1';
+import { metricLength } from '../src/modif1';
+import { metricMass } from '../src/modif1';
+import { Adapter } from '../src/modif1';
 
 describe('prueba modif1', () =>{
-  it('primer test', () => {
+  let centimetros = new metricLength(1);
+  it('primer test CM a M', () => {
+    expect(centimetros.getMetros()).to.be.equal(0.01);
+  });
 
+  it('primer test cm a km', () => {
+    expect(centimetros.getKM()).to.be.equal(0.000009999999999999999);
   });
     
+  let gr = new metricMass(1);
+  it('primer test gr a kgr', () => {
+    expect(gr.getKiogramos()).to.be.equal(0.001);
+  });
+  
+  it('primer test gr a tn', () => {
+    expect(gr.getToneladas()).to.be.equal(0.000001);
+  });
+
+  let pulgadas = new imperialLength(1)
+  it('primer test pulgadas a yardas', () => {
+    expect(pulgadas.getYardas()).to.be.equal(0.0277778);
+  });
+
+  it('primer test pulgadas a pies', () => {
+    expect(pulgadas.getPies()).to.be.equal(0.0833333);    
+  });
+
+  let oz = new imperialMasa(1)
+  it('primer test oz a libras', () => {
+    expect(oz.getLibras()).to.be.equal(0.0625);    
+  });
+
+  it('primer test oz a piedras', () => {
+    expect(oz.getPiedras()).to.be.equal(0.00446429);    
+  });
+  
+  let conversion = new Adapter(new metricLength(1))
+
+  it('primer test oz pasar de pulgadas a cm', () => {
+    expect(conversion.getData()).to.be.equal(2.54);    
+  });
+
 });
